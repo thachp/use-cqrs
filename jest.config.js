4fb7a0a384e376cab3992a7ce0bbc52e3bcd5e4b
Patrick Thach <thachp@gmail.com>
2022-01-03 22:36:34 -0800

    Added react to devDepencies.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
     preset: "ts-jest",
+    testPathIgnorePatterns: ["*.spec.js", "*.spec.d.ts"],
     testEnvironment: "jsdom"
 };

2ab6a9bd060f46339fc2e37fc395bcb9ffc86a03
Patrick Thach <thachp@gmail.com>
2022-01-08 20:42:35 -0800

    Renamed cqrs.provider.tsx to cqrs.provider.ts

--- dist/examples/events/components/ExampledExceptionEvent.component.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="react" />
-export declare const ExampledExceptionEventComponent: () => JSX.Element;
--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "./cqrs/operators/of-name";
-export * from "./command.hook";
-export * from "./event.hook";
-export * from "./query.hook";
-export { Container as IoC, Service as Injectable } from "typedi";
